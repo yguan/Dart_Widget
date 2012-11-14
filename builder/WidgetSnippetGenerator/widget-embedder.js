6019b7001a5d4f0a10b9cbaf4d42f5bb17412ec0
John Emau <john.emau@gmail.com>
2012-11-14 11:37:07 -0800

    added widget snippet generator and moved the embedder

---
+++ builder/WidgetSnippetGenerator/widget-embedder.js
@@ -0,0 +1,18 @@
+// Widget Embedder
+// Appends widgets into the dom
+
+(function() {
+	var portalElement = $('[data-portal="project"]');
+	var project = portalElement.attr('data-project');
+	
+	// Test Code
+	var server = sinon.fakeServer.create();
+	server.respondWith([200, { "Content-Type": "text/html" },
+                                 '<h1>My Awesome Project</h1>']);
+	server.autoRespond = true;
+	// End Test Code
+	
+	$.get('my.fake.server', function(portalData){
+		portalElement.append(portalData);	
+	});
+})();
