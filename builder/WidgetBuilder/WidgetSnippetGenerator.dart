a4da6ce8762ee8ac2a91024bb96df325ea8776b8
John Emau <john.emau@gmail.com>
2012-11-14 13:38:04 -0800

    Added the Widget Snippet Generator dart file, a basic working version

---
+++ builder/WidgetBuilder/WidgetSnippetGenerator.dart
@@ -0,0 +1,11 @@
+#library("WidgetSnippetGenerator")
+
+class WidgetSnippetGenerator {
+  String snippetBase = '<div data-portal="project" data-project="SomeProject" data-height="{{height}}" data-width="{{width}}"></div>';
+  
+  CreateSnippet(int height, int width) {
+    return this.snippetBase
+        .replaceAll('{{height}}', height.toString())
+        .replaceAll('{{width}}', width.toString());
+  }
+}
