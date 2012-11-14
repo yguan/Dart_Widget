2a614fefd86687db57148d47a5b5add24b157e8b
John Emau <john.emau@gmail.com>
2012-11-14 14:05:12 -0800

    added script tag to snippet generator

--- builder/WidgetBuilder/WidgetSnippetGenerator.dart
+++ builder/WidgetBuilder/WidgetSnippetGenerator.dart
@@ -1,11 +1,26 @@
 library WidgetSnippetGenerator;
 
 class WidgetSnippetGenerator {
-  String snippetBase = '<div data-portal="project" data-project="SomeProject" data-height="{{height}}" data-width="{{width}}"></div>';
+   
+  var widgetEmbbederTag = """ 
+  <script src='http://code.jquery.com/jquery-1.8.2.min.js'></script>
+  <script>
+        (function(d){
+         var js, id = 'widget', ref = d.getElementsByTagName('script')[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "widget-embedder.js";
+         ref.parentNode.insertBefore(js, ref);
+         }(document));
+   </script>
+   """;   
+  var snippetBase = '<div data-portal="project" data-project="SomeProject" data-height="{{height}}" data-width="{{width}}"></div>';
   
   CreateSnippet(int height, int width) {
-    return this.snippetBase
+    var divSnippet = this.snippetBase
         .replaceAll('{{height}}', height.toString())
         .replaceAll('{{width}}', width.toString());
+    
+    return widgetEmbbederTag.concat(divSnippet);
   }
 }
